1de4033af71f8416c2d04195b836a4e63b6d524b
evan <evan@sdicg.com>
2015-02-17 19:24:58 -0500

    ready to start the history viewer

--- index.js
+++ index.js
@@ -4,28 +4,30 @@ var qs = require('querystring');
 var fs = require('q-io/fs');
 var parseurl = require('parseurl');
 var _ = require('underscore');
-
+var bodyParser = require('body-parser');
 
 function reporter(log_location){
   return function (req, res, next) {
     var url = parseurl(req)
       , query = qs.parse(url.query)
       , info = {}
-      , timestamp = +(new Date());
+      , timestamp = +(new Date())
+      , log_to
       ;
 
     info.method       = req.method; 
     info.url          = url.pathname; 
     info.body         = req.body; 
+    info.headers      = req.headers; 
     info.query_string = query;
 
-    log_location += '/'+timestamp+'-'+info.method+info.url.replace(/\//g, "\\") +'.json';
+    log_to = log_location + timestamp+'-'+info.method+info.url.replace(/\//g, "\\") +'.json';
     
     // ignore __log entries
     if(info.url.match('__log')){
       next();
     }else{
-      fs.write(log_location, JSON.stringify(info, null, "\t"))
+      fs.write(log_to, JSON.stringify(info, null, "\t"))
         .fail(function(err){
           console.log(err);
         })
