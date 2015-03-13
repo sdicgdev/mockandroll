5d9bd732f13d534b6503546419604807d89f8d0e
jared <jared@sdicg.com>
2015-03-13 15:45:00 -0400

    fixed missing js; html still broken

--- ui/modules/mockandroll-controller.js
+++ ui/modules/mockandroll-controller.js
@@ -2,30 +2,35 @@
 
 angular.module('MockAndRollApp')
   .controller('MockAndRollCtl', function ($scope, History) {
-  	// initialize a container to hold the item details	
-  	$scope.details = {	}
-  	// ## load history data
-  	History.query()
+    // initialize a container to hold the item details  
+    // ## load history data
+    History.query()
       .$promise.then(function(data){
-      	//set history records in current scope to returned values
-      	$scope.history = data;
-      	//copy object to console
+        //set history records in current scope to returned values
+        $scope.history = data;
+        //copy object to console
         console.warn(data);
       });
       // ### clear.History
       // clear out the history
       $scope.clearHistory = function() {
-      		// stub
+          // stub
       };
       // ### showDetails
 
-      $scope.showDetails = function(item) {
-			  console.log(item);
-			  $scope.detailsVisible = true;
-			  $scope.details = item;
-			};
+      $scope.displayDetails  = function(item, index){
+        showDetails(item);
+        select(index);
+      }
 
-    	$scope.select= function(index) {
+      /*** private methods ***/
+
+      function showDetails(item) {
+        console.log(item);
+        $scope.details = item;
+      };
+
+      function select(index) {
         $scope.selected = index; 
-    	};
+      };
   });
